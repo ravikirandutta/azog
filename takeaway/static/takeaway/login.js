83d5ea81b939c07ac57f3d2225f1e96a48be1ed3
sagardutta <f2003484@gmail.com>
2014-07-02 21:01:49 -0500

    referencing css and js resources using static loader

---
+++ takeaway/static/takeaway/login.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+$("#new_user").click(function(){
+  if ($('#new_user').val() == 'Register') {
+    $('#mode').val ("Register") ;
+    //alert("Inside register If");
+    //alert("Mode values is : " + $('#mode').val());
+    $("#login_form").submit();
+  } else {
+  $('#sync').addClass('registerMode');
+  var speed=220;
+   $(' .email').animate({top:'13px'},speed);
+  $('#sync .new_user').animate({top:'70px'},speed);
+  $('#sync .password').animate({top:'43px'},speed);
+  $('#sync .spinner').animate({top:'370px'},speed);
+  $('#sync > span').animate({top:'125px'},speed);
+  $('#new_user').val ("Register") ;
+}
+});
+
+});
